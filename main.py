f031083ce50e8387307130f8b4a48f772843514e
ivan <ivan.bondarenko.it@gmail.com>
2022-12-03 14:12:37 +0400

    9.1(done), 9.2(done), 9.3

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-'''Домашнее задание
-
-
-3) Для класса Box напишите статический метод, который будет подсчитывать
-суммарный объем двух ящиков, которые будут его параметрами.'''
-
-
 # 1) Создайте декоратор, который зарегистрирует декорируемый класс в
 # списке классов.
 
@@ -26,18 +19,17 @@ def class_register(cls):
 def left_str(str_to_add):
     def decorator(func):
         def inner(*args, **kwargs):
-            return f"{str_to_add}+{func(*args, **kwargs)}"
+            return f"{str_to_add}{func(*args, **kwargs)}"
         return inner
     return decorator
 
 
 def left_str_adder(str_to_add):
     def decorator(cls):
-        def inner(*args, **kwargs):
+        cls.__str__ = left_str(str_to_add=str_to_add)(cls.__str__)
 
+        def inner(*args, **kwargs):
             new_instance = cls(*args, **kwargs)
-            cls.__str__ = left_str(str_to_add=str_to_add)(cls.__str__)
-            # new_instance.__str__ = left_str(str_to_add=str_to_add)(new_instance.__str__)
             return new_instance
 
         return inner
@@ -45,8 +37,8 @@ def left_str_adder(str_to_add):
     return decorator
 
 
-@left_str_adder(str_to_add="***")
-# @class_register
+@class_register
+@left_str_adder(str_to_add="*****")
 class Box:
     def __init__(self, x, y, z):
         self.x = x
@@ -56,14 +48,22 @@ class Box:
     def __str__(self):
         return "Box [x = "+str(self.x)+", y = "+str(self.y)+", z = "+str(self.z)+" ]"
 
-
-b = Box(1, 3, 5)
-b1 = Box(2, 4, 6)
-b2 = Box(2, 4, 6)
-print(class_list)
-print(b)
-print(b)
-print(b1)
-print(b1)
+    # 3) Для класса Box напишите статический метод, который будет подсчитывать
+    # суммарный объем двух ящиков, которые будут его параметрами.
+    @staticmethod
+    def box_and_box(self):
+        return None
 
 
+def main():
+    b = Box(1, 3, 5)
+    b1 = Box(2, 4, 6)
+    print(class_list)
+    print(str(b))
+    print(b)
+    print(b1)
+    print(b1)
+
+
+if __name__ == "__main__":
+    main()
